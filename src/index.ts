d2ac4b596773db34fa48f6b08dc419d0ebd44153
Abayomi Ayodele <samabayomi301@gmail.com>
2023-08-04 07:04:43 +0100

    Initial API configs

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const main = async () => {
+  const app = express();
+
+  app.get("/", (_req, res) => {
+    res.send("hello");
+  });
+
+  app.listen(3002, () => {
+    console.log("Listening on localhost:3002");
+  });
+};
+
+main();
